dc9ef8a7a251cf69dd1389f61ba48a9c7c5e2898
mike last <pugsyblack@hotmail.com>
2019-02-02 14:13:11 -0800

    Updates

---
+++ precache-manifest.6a76a191ddb5a1216f117ad2eaf05402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "793389e325fe0a35b5208e1fc0067dac",
+    "url": "/pomodoro/static/media/alarm.793389e3.wav"
+  },
+  {
+    "revision": "a702e67ff471c6ecfc33",
+    "url": "/pomodoro/static/js/runtime~main.a702e67f.js"
+  },
+  {
+    "revision": "8349a6e728591f9fb486",
+    "url": "/pomodoro/static/js/main.8349a6e7.chunk.js"
+  },
+  {
+    "revision": "c73fceecd1c5109bdaf3",
+    "url": "/pomodoro/static/js/1.c73fceec.chunk.js"
+  },
+  {
+    "revision": "8349a6e728591f9fb486",
+    "url": "/pomodoro/static/css/main.67befd76.chunk.css"
+  },
+  {
+    "revision": "b918b1a2174ac4ebe1c0272eeb9ed895",
+    "url": "/pomodoro/index.html"
+  }
+];
