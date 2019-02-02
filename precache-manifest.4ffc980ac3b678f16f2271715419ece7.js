3679fd43ae1e9e7a920654d794538ee7aeb7a49c
mike last <pugsyblack@hotmail.com>
2019-02-02 13:49:17 -0800

    first commit

---
+++ precache-manifest.4ffc980ac3b678f16f2271715419ece7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "793389e325fe0a35b5208e1fc0067dac",
+    "url": "/static/media/alarm.793389e3.wav"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c6d0ee423bff5a3239b2",
+    "url": "/static/js/main.c6d0ee42.chunk.js"
+  },
+  {
+    "revision": "c73fceecd1c5109bdaf3",
+    "url": "/static/js/1.c73fceec.chunk.js"
+  },
+  {
+    "revision": "c6d0ee423bff5a3239b2",
+    "url": "/static/css/main.67befd76.chunk.css"
+  },
+  {
+    "revision": "c059954884836b6c3c419844b1b62528",
+    "url": "/index.html"
+  }
+];
